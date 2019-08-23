d6b662f558b950981808ad496e5e739176c69cdd
Noel Nunkovich <noel.nunkovich@veolia.com>
2019-08-23 16:44:10 -0400

    add routes for categories

--- ChaoticFocus/routes.py
+++ ChaoticFocus/routes.py
@@ -23,4 +23,36 @@ posts = [
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('home.html', title='Chaotic Focus')
+    return render_template('home.html', title='Chaotic Focus', posts=posts)
+
+@app.route('/about')
+def about():
+    render_template('about.html', title='About Me')
+
+@app.route('/general')
+def category_general():
+    pass
+
+@app.route('/tech')
+def category_tech():
+    pass
+
+@app.route('/cycling')
+def category_cycling():
+    pass
+
+@app.route('/woodworking')
+def category_woodworking():
+    pass
+
+@app.route('/smart-fan')
+def proj_smartfan():
+    pass
+
+@app.route('/led_cube')
+def proj_led_cube():
+    pass
+
+@app.route('/dog_bed')
+def proj_dog_bed():
+    pass
